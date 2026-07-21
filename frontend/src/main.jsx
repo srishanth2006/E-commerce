69c5e8022072f3f53f1106d410103e12a507f88e
Sri Shanth <srishanth244@gmail.com>
2026-07-22 00:16:37 +0530

    Remove service worker causing blank page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,8 +12,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
-  });
-}
+
